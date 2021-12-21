5dadb0f0d7ef6f0b09f4b132f9e7d728de7a19c6
fabsantandrea <fabsantandrea@gmail.com>
2021-12-21 13:48:10 -0300

    cambie height del screen

--- client/src/Styles/BackgroundColor.js
+++ client/src/Styles/BackgroundColor.js
@@ -1,12 +1,9 @@
-
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const BackgroundColor = styled.div`
+  background: #d3e4cd;
+  width: 100%;
+  height: 100vh;
+`;
 
-background:#D3E4CD;
-width:100% ;
-min-height: 721px;
-
-`
-
-export default BackgroundColor
+export default BackgroundColor;
